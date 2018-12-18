8e061da16a2b5eea5e4c8ca458c2ddefc3e88601
Oguz Kazkayasi <mkazkayasi3@gatech.edu>
2018-12-18 21:53:54 +0300

    reran

--- 09_HW_8_Strategy_Learner/experiment1.py
+++ 09_HW_8_Strategy_Learner/experiment1.py
@@ -7,5 +7,4 @@ learner = sl.StrategyLearner(verbose = False, impact = 0.00) # constructor
 learner.addEvidence("IBM", dt.datetime(2008,1,1), dt.datetime(2009,12,31), 100000)
 
 
-
 ms.test_policy(['IBM'], dt.datetime(2009,1,1), dt.datetime(2010,12,31), sv=100000)
